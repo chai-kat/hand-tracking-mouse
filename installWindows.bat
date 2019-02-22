622b617bf63500f3974309bde38acbc1aaf349dc
Chaitanya KATPATAL <chaitanyakatpatal@Chaitanyas-MacBook-Air.local>
2019-02-22 18:23:42 +0100

    Fixed Windows Installer to work natively

--- installWindows.bat
+++ installWindows.bat
@@ -2,16 +2,12 @@ mkdir tmpInstallHtMouse
 copy environment.yml tmpInstallHtMouse
 cd tmpInstallHtMouse
 
-# Make sure the flags are the same.
-curl -fSL https://repo.anaconda.com/miniconda/Miniconda2-latest-Windows-x86_64.exe > miniconda_installer.exe
-
-cmd miniconda_installer.exe
+bitsadmin /transfer "Miniconda Download" /download /priority normal https://repo.anaconda.com/miniconda/Miniconda2-latest-Windows-x86_64.exe .\miniconda_installer.exe
+miniconda_installer.exe
 
 conda env create --file=environment.yml
 
 echo "Removing installation files."
 cd ..
-
 rmdir /s /q tmpInstallHtMouse
-
 echo "Install completed successfully."
